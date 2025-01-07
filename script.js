b348f36d2c63579aac549a9e362b1614b124c1ee
J04N4 <joanaqmendonca@hotmail.com>
2025-01-07 14:57:52 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("mude-button").onclick = function () {
+    window.open("https://www.behance.net/gallery/215565079/Redesign-MUDE-website/modules/1227085705", '_blank').focus();
+};
+
+document.getElementById("moey-button").onclick = function () {
+    window.open("https://www.behance.net/gallery/215992229/Apresentacao-projeto-final-360/modules/1229656913", '_blank').focus();
+};
